266426da3aa6d8d36d03cc5e24d84e03bbed7602
umiitkose <49441806+umiitkose@users.noreply.github.com>
2023-08-18 11:35:57 +0300

    Update WebConfig.java

--- crud-backend/src/main/java/com/umiitkose/crudbackend/security/WebConfig.java
+++ crud-backend/src/main/java/com/umiitkose/crudbackend/security/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // your reactjs URL
+                .allowedOrigins("http://localhost:5173") // your reactjs URL //if you use cra please change port.
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE","OPTIONS")
                 .allowedHeaders("*") // Adjust headers you need to allow
                 .allowCredentials(true); // Add only if you want to access cookie
